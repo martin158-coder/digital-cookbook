9daafff8063d3ac5d4a16b8bf0c259d648dc86fe
martin158 <martinbv3125@gmail.com>
2026-08-08 14:32:57 -0600

    Use Render port for production

--- app.py
+++ app.py
@@ -277,5 +277,5 @@ app.register_blueprint(recipes_bp)
 if __name__ == "__main__":
     app.run(
         host=os.getenv("HOST", "127.0.0.1"),
-        port=5000
+        port=int(os.getenv("PORT", "5000"))
     )
